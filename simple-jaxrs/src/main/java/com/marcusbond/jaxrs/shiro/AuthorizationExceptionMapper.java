b75c633b1ac545b3776a82a32624b3484ccc0c38
Marcus <Marcus@MBelitebook>
2012-08-01 21:46:45 +0100

    Added JavaDoc

--- simple-jaxrs/src/main/java/com/marcusbond/jaxrs/shiro/AuthorizationExceptionMapper.java
+++ simple-jaxrs/src/main/java/com/marcusbond/jaxrs/shiro/AuthorizationExceptionMapper.java
@@ -7,8 +7,17 @@ import javax.ws.rs.ext.Provider;
 
 import org.apache.shiro.authz.AuthorizationException;
 
+/**
+ * Provides a means for the JAX-RS implementation to handle unchecked Shiro
+ * {@link AuthorizationException}s and instead of returning an internal error
+ * return a 403 Forbidden response code.
+ * 
+ * @author Marcus Bond
+ * 
+ */
 @Provider
-public class AuthorizationExceptionMapper implements ExceptionMapper<AuthorizationException> {
+public class AuthorizationExceptionMapper implements
+		ExceptionMapper<AuthorizationException> {
 
 	@Override
 	public Response toResponse(AuthorizationException exception) {
